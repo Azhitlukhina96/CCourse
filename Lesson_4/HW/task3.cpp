1763bfd67e1f152234984e67fd8cb787f27aaa3a
Azhitlukhina96 <31824816+Azhitlukhina96@users.noreply.github.com>
2017-10-02 22:45:09 +0300

    Create task3.cpp

---
+++ Lesson_4/HW/task3.cpp
@@ -0,0 +1,28 @@
+// ConsoleApplication12.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <math.h>
+
+
+int main()
+{
+	int min = 0;
+	int max= 0;
+	int x = 0; 
+	printf("Enter min:\n");
+    scanf_s("%d", &max);
+	printf("Enter max:\n");
+    scanf_s("%d", &max);
+	if (min > max)
+	{
+		printf("Incorrect! min > max");
+		return 0;
+	}
+	for (int i = min; i <= max; i++)
+	{
+		x = x + i;
+	}
+	printf("Sum:%d; \n", x);
+	return 0;
+}
