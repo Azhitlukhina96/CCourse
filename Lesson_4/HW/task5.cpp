6f5dc408b9f6247e3ce9e98993611736d185e751
Azhitlukhina96 <31824816+Azhitlukhina96@users.noreply.github.com>
2017-10-05 15:26:10 +0300

    Create task5.cpp

---
+++ Lesson_4/HW/task5.cpp
@@ -0,0 +1,22 @@
+// ConsoleApplication14.cpp: определяет точку входа для консольного приложения.
+//
+#include "stdafx.h"
+
+
+
+int main()
+{
+	int a = 0;
+	int	count = 0;
+	printf("Vvedite luboe chislo:");
+	scanf_s("%d", &a);
+	for (int i = 2; i<a; i++) 
+	{
+		if (a%i == 0) count++;
+	}
+	if (count == 0)
+		printf("chislo prostoe\n");
+	else
+		printf("Chislo ne prostoe\n");
+	return 0;
+}
