ce52d858c71db96d73a000fed70a4ab640b10a06
Azhitlukhina96 <31824816+Azhitlukhina96@users.noreply.github.com>
2017-10-07 12:39:43 +0300

    Create task5.cpp

---
+++ Lesson_5/CW/task5.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#define A 5
+#define B 3
+
+
+
+int main()
+{
+	printf("%d+ %d = %d", A, B, A+B);
+	return 0;
+}
