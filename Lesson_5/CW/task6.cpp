ea870565d7c56811ed5c5d352adab26ef56b47a5
Azhitlukhina96 <31824816+Azhitlukhina96@users.noreply.github.com>
2017-10-07 12:48:36 +0300

    Create task6.cpp

---
+++ Lesson_5/CW/task6.cpp
@@ -0,0 +1,17 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#define sum( a , b ) (a + b)
+
+
+int main()
+{
+	int value_1 = 0;
+	int value_2 = 0;
+	int result = 0;
+	scanf_s("%d", &value_1);
+	scanf_s("%d", &value_2);
+	result = sum(value_1, value_2);
+	printf("sum= %d\n", result);
+}
