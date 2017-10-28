3ed11715ca1a8aa6b25a059ed290417cea2f5534
Azhitlukhina96 <31824816+Azhitlukhina96@users.noreply.github.com>
2017-10-28 10:51:53 +0300

    Create task2.cpp

---
+++ Lesson_8/CW/task2.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+
+
+int main()
+{
+	int age = 15;
+	int steve_age = 0;
+	int *ptr_age = NULL;
+	ptr_age = &age;
+	steve_age = *ptr_age;
+	printf("%d\n", age);
+	printf("%d\n", ptr_age);
+	printf("%d\n", steve_age);
+	return 0;
+}
