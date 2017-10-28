3c1060c71b4793b4a96998236c7d756c9a9aa01f
Azhitlukhina96 <31824816+Azhitlukhina96@users.noreply.github.com>
2017-10-28 10:55:39 +0300

    Create task3.cpp

---
+++ Lesson_8/CW/task3.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+
+int main()
+{
+	int ann_book = 12;
+	int *ptr_ann = NULL;
+	int *ptr_mike = NULL;
+	ptr_ann = &ann_book;
+	ptr_mike = ptr_ann;
+	printf("%d\n", ann_book);
+	printf("%d\n", *ptr_ann);
+	printf("%d\n", *ptr_mike);
+	return 0;
+}
