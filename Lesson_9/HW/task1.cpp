94fefca4379c18fbe8566708a05657163533c4f0
Azhitlukhina96 <31824816+Azhitlukhina96@users.noreply.github.com>
2017-11-24 21:22:12 +0300

    Create task1.cpp

---
+++ Lesson_9/HW/task1.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+	char *my_string_1 = NULL;
+	int string_len = 0;
+	my_string_1 = (char *)malloc(sizeof(char)*string_len);
+
+	char in_char = 0;
+	int position = 0;
+
+
+	int colichestvo = 0;
+	while ((in_char = getchar()) != EOF && position < string_len - 1)
+	{
+		my_string_1[position] = in_char;
+		if (position == string_len - 2)
+		{
+			my_string_1[string_len - 1] = '\0';
+			break;
+		}
+		position++;
+
+	}
+	for (int i = 0; i < string_len; i++)
+	{
+
+		colichestvo = 0;
+		if (my_string_1[i] = 'a')
+			colichestvo = colichestvo + 1;
+
+	}
+
+	printf("%d\n", colichestvo);
+
+	return 0;
+}
