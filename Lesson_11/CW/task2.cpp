815876358f17ee71e43fe6cd502d716e15ddab81
Azhitlukhina96 <31824816+Azhitlukhina96@users.noreply.github.com>
2017-12-09 10:49:05 +0300

    Update task2.cpp

--- Lesson_11/CW/task2.cpp
+++ Lesson_11/CW/task2.cpp
@@ -1,8 +1,56 @@
 #include "stdafx.h"
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+#include <time.h>
 #define BUFFER_SIZE 1024
-using namespace std
+#include <string.h>
+//сортировка простыми вставками
+
+void InsertSort(float a[], int size)
+{
+	float buf = 0;
+	float k = 0;
+	int position = 0;
+
+	printf("\n--------\n");
+
+	size--;
+
+	printf("\nsize: %d\n", size);
+
+	for (int i = 0; i < size; ++i)
+	{
+		buf = a[i];
+
+		for (int j = i - 1; j >= 0; --j)
+		{
+			if (buf < a[j]) // Поиск минимума
+			{
+
+				buf = a[j + 1];
+				position = j;
+			}
+			
+		}
+		
+		for ()
+
+		// Вставка в отсортированную последовательность
+
+		// нужно добавить цикл для вставки в последовательность от 0 до i
+		k = a[i];
+		a[i] = buf;
+		a[position] = k;
+
+		for (int m = 0; m < size; m++)
+		{
+			printf("{%.0f}", a[m]);
+		}
+
+
+		printf("\n\n\n");
+	}
+}
 
 
 
@@ -10,33 +58,36 @@ int main()
 {
 
 	FILE *array2;
-	float value = 0;
+
 	float *row = NULL;
-    row = (float*)malloc(sizeof(float));
-	float *brow = NULL;
 	int size = 1;
-	int position = 0;
+	float *brow = NULL;
+
+	row = (float*)malloc(sizeof(float));
+
+
 	errno_t err = fopen_s(&array2, "C:\\Users\\uc2\\Desktop\\array2.txt", "r");
 	if (err)
 		printf_s("The file array2.txt was not opened\n");
 	else
 	{
-		while (fscanf_s(array2, "%f", &value) != EOF) {
-			if (position==size)
-			{
-				size = size * 2;
-				brow = (float *)realloc(row, size*sizeof(float));
-				for (int i = 0; i < position; i++)
-					brow[i] = row[i];
-				row = brow;
-			}
-			row[position] = value;
-			position++;
-			printf("%f\n", value);
+		int a = 0;
+		while (fscanf_s(array2, "%f", &row[a]) != EOF)
+		{
+			printf(" %.0f ", row[a]);
+			a++;
+			size++;
+
+			brow = (float*)realloc(row, size * sizeof(float));
+			row = brow;
 		}
 	}
-	
 
-	
-		return 0;
+	printf("\n");
+
+	InsertSort(row, size);
+
+	printf("\n");
+
+	return 0;
 }
