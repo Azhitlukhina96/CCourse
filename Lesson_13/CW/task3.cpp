c75b10b977e4a1149c18210d21a4905a02898887
Azhitlukhina96 <31824816+Azhitlukhina96@users.noreply.github.com>
2017-12-18 23:15:47 +0300

    Update task3.cpp

--- Lesson_13/CW/task3.cpp
+++ Lesson_13/CW/task3.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <conio.h>
 #define BUFFER_SIZE 1024
 
 struct point
@@ -15,7 +16,7 @@ struct cloud
 	struct point points[10];
 };
 
-float distance(struct point point1, struct point point2)
+float distance(point point1, point point2)
 {
 	float result = 0;
 	result = sqrt((point1.x - point2.x)*(point2.x - point1.x) + sqrt(point2.y - point1.y)*(point2.y - point1.y));
@@ -28,29 +29,44 @@ int main()
 	FILE *cloud_file;
 	char buffer[BUFFER_SIZE];
 
-	struct point best_point;
+	//struct point best_point;
 	struct cloud my_cloud;
+	struct point my_coord;
 
-	best_point.x = 0;
-	best_point.y = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		my_cloud.points[i].x = 0;
+		my_cloud.points[i].y = 0;
+	}
 
 
-	errno_t err = fopen_s(&cloud_file, "C:\\Users\\uc2\\Desktop\\cloud.txt", "r");
+	errno_t err = fopen_s(&cloud_file, "C:\\Users\\1\\Desktop\\cloud.txt", "r");
 
 	if (err)
 		printf_s("The file cloud.txt was not opened\n");
 	else
 	{
 		int counter = 0;
-		while (fscanf_s(cloud_file, "%f %f", &best_point.x, &best_point.y) != EOF)
+		while (fscanf_s(cloud_file, "%f %f", &my_cloud.points[counter].x, &my_cloud.points[counter].y) != EOF)
 		{
-			my_cloud.points[counter] = best_point;
-			printf("x: %f\n", my_cloud.points[counter].x);
-			printf("y: %f\n", my_cloud.points[counter].y);
-			
-			counter++;
-			}
-		}
 
+			printf_s("x: %f", my_cloud.points[counter].x);
+			printf_s("    y: %f\n", my_cloud.points[counter].y);
+			
+
+			counter++;
+		}
+	};
+
+	//написать функцию ввода координаты x,y с клавиатуры. (scan_f) --> my_coord;
+
+
+	//вызвать функцию через for(10) distance(my_coord, my_cloud.points[i])
+
+	
+
+	//float distance;
+	_getch();
 	return 0;
 }
+
