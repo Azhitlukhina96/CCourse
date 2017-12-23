94da44576779b61afa0e1521e19e415b88961e47
Azhitlukhina96 <31824816+Azhitlukhina96@users.noreply.github.com>
2017-12-23 12:32:59 +0300

    Update task3.cpp

--- Lesson_13/CW/task3.cpp
+++ Lesson_13/CW/task3.cpp
@@ -18,6 +18,37 @@ void print_my_coord(struct point my_coord)
 
 }
 
+void print_points_dictance_array(struct point my_coord_array[], int size)
+{
+	for (int i = 0; i < size; i++)
+		print_my_coord (my_coord_array[i]);
+}
+
+
+
+
+//Сортировка массива пузырьком
+void sort(float points_distances_array[], int *points_id, int size)
+{
+	for (int i = 0; i < size - 1; i++) 
+	{
+		for (int j = 0; j < size - i - 1; j++) 
+		{
+			if (points_distances_array[j] > points_distances_array[j + 1])
+			{
+				float temp = points_distances_array[j];
+				int temp_id = points_id[j];
+				points_distances_array[j] = points_distances_array[j + 1];
+				points_id[j] = points_id[j + 1];
+
+				points_distances_array[j + 1] = temp;
+				points_id[j + 1] = temp_id;
+			}
+		}
+	}
+}
+
+
 struct cloud
 {
 	struct point points[10];
@@ -35,6 +66,7 @@ int main()
 {
 	FILE *cloud_file;
 	char buffer[BUFFER_SIZE];
+	int size = 10;
 
 	//struct point best_point;
 	struct cloud my_cloud;
@@ -70,17 +102,37 @@ int main()
 	scanf_s("%f", &my_coord.y);
 
 	print_my_coord(my_coord);
-	
+
 	float points_distance;
+	float distances_array[10];
+	int points_id[10];
 
 	for (int i = 0;i < 10;i++)
 	{
-		points_distance = distance(my_coord, my_cloud.points[i]);
+		points_distance = distance(my_coord, my_cloud.points[i]); 
 		printf("%f\n", points_distance);
+		distances_array[i] = points_distance;
+		points_id[i] = i;
 	}
+
+
+
+
 	
-	
+	sort(distances_array, points_id, size);
+	for (int i = 0;i < 10;i++)
+	{
+		printf("%d: %f\n", points_id[i], distances_array[i]);
+	}
+
+	/*
+	// Простой вывод результата
+	print_points_dictance_array(my_cloud.points, size);
+	//вывод отсортированного массива
+	print_points_dictance_array(distances_array, size);
+	*/
 	
 	_getch();
 	return 0;
 }
+
