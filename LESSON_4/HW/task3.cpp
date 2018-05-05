af71a9dd9ca3f6601a7a80f336a76229f727218b
Azhitlukhina96 <31824816+Azhitlukhina96@users.noreply.github.com>
2018-05-05 11:36:52 +0300

    Update task3.cpp

--- LESSON_4/HW/task3.cpp
+++ LESSON_4/HW/task3.cpp
@@ -155,6 +155,24 @@ int getDepth(Node *root, int depth)
 	return depth - 1;
 }
 
+void w_tree(struct Node* root, int *w, int level)
+{
+	if (root) {
+		/*
+		if (root->left != NULL) {
+			w[level] += 1;
+		}
+		if (root->right != NULL) {
+			w[level] += 1;
+		}
+		*/
+		w[level - 1] += 1;
+		w_tree(root->left, w, level + 1);
+		w_tree(root->right, w, level + 1);
+	}
+}
+
+
 
 int main() {
 	Node *root = NULL;
@@ -176,8 +194,17 @@ int main() {
 	printf("Sorted tree: \n");
 	printSortedTree(root, "root", 0);
 	getDepth(root, 0);
-	printf("Tree depth: %d", getDepth(root, 0));
+	printf("Tree depth: %d\n", getDepth(root, 0));
+	
 
+	int *w;
+	w = (int *)malloc(20 * sizeof(int));
+	memset(w, 0, 20 * sizeof(int));
+	w_tree(root, w, 0);
+	for (int i = 0; i < 20; i++) {
+		printf("W:  %d  Level:  %d\n", w[i], i + 1);
+	}
 	return 0;
 }
+
 
