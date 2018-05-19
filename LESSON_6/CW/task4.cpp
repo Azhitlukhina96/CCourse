ff1755afbf548b20bba431bf76e2eef60b0923c1
Azhitlukhina96 <31824816+Azhitlukhina96@users.noreply.github.com>
2018-05-19 13:06:41 +0300

    Create task4.cpp

---
+++ LESSON_6/CW/task4.cpp
@@ -0,0 +1,132 @@
+#include "stdafx.h"
+#include <stdlib.h>
+
+class queue {
+protected:
+	//int _q[100];
+	int *_q;
+	int _queue_size;
+	int _sloc;
+public:
+	queue(); // конструктор
+	~queue(); // деструктор
+	void print();
+	void push(int i);
+	int pop();
+	int count();
+	int get(int);
+};
+
+// конструктор
+queue::queue()
+{
+	_queue_size = 100;
+	_q = (int *)malloc(sizeof(int) * _queue_size);
+	_sloc = 0;
+	printf("Queue initialized.\n");
+}
+
+// деструктор
+queue ::~queue()
+{
+	printf("Queue destroyed.\n");
+}
+
+// Добавление элемента в конец очереди
+// внимательно изучите эту функцию
+void queue::push(int i)
+{
+	if (_sloc == 99) {
+		printf("Queue is full.\n");
+		return;
+	}
+	_q[_sloc] = i;
+	_sloc++;
+}
+
+//функция вывода элементов очереди / стека в консоль
+void queue::print()
+{
+	for (int i = 0; i < _sloc; i++)
+	{
+		printf("Queue:  %d\n", _q[i]);
+	}
+
+}
+
+// поиск длины очереди/стека
+int queue::count()
+{
+	return _sloc;
+}
+
+// получениe элемента по индексу 
+
+int queue::get(int id)
+{
+	return _q[id];
+}
+
+//Реализaция динамического изменения размера очереди 
+
+
+
+
+// Удаление элемента из начала очереди
+int queue::pop() {
+	if (_sloc == 0) {
+		printf("Queue underflow.\n");
+		return 0;
+	}
+	int value = _q[0];
+
+	for (int i = 0; i < _sloc; i++) {
+		_q[i] = _q[i + 1];
+	}
+	_sloc--;
+	return value;
+}
+
+
+class stack : public queue
+{
+public:
+	int pop();
+};
+int stack::pop() {
+	if (_sloc == 0) {
+		printf("stack underflow.\n");
+		return 0;
+	}
+	return _q[--_sloc];
+}
+
+
+int main()
+{
+	//queue MyQueue;
+	queue MyQueue;
+
+	MyQueue.push(1);
+	MyQueue.push(2);
+	MyQueue.push(3);
+	MyQueue.push(4);
+
+	MyQueue.print();
+//	MyQueue.count();
+	printf("ALL_Queue: %d\n", MyQueue.count());
+	printf("Element of an index: %d\n", MyQueue.get(1));
+
+
+	/*
+	printf("%d\n", MyQueue.pop());
+	printf("%d\n", MyQueue.pop());
+	printf("%d\n", MyQueue.pop());
+	printf("%d\n", MyQueue.pop());
+	printf("%d\n", MyQueue.pop());
+	*/
+
+
+	return 0;
+}
+
